0845b3d248336e4a36041b919c23775f58f29979
Carlos Blanco <carlosblanco@me.com>
2022-09-26 18:28:38 +0200

    Add card array

--- js/index.js
+++ js/index.js
@@ -1,3 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
   
+  const cardArray = [
+    {
+      name: 'One',
+      img: '/images/1.jpg'
+    },
+    {
+      name: 'Two',
+      img: '/images/2.jpg'
+    },
+    {
+      name: 'Three',
+      img: '/images/3.jpg'
+    },
+    {
+      name: 'Four',
+      img: '/images/4.jpg'
+    },
+    {
+      name: 'Five',
+      img: '/images/5.jpg'
+    },
+    {
+      name: 'Six',
+      img: '/images/6.jpg'
+    },
+    {
+      name: 'Seven',
+      img: '/images/7.jpg'
+    },
+    {
+      name: 'Eight',
+      img: '/images/8.jpg'
+    },
+    {
+      name: 'Nine',
+      img: '/images/9.jpg'
+    },
+    {
+      name: 'Ten',
+      img: '/images/10.jpg'
+    }
+  ]
 })
